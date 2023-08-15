372e2995c08244962a724ed873e71f285f4bc60b
Adam Snow <adamjohnsnow@icloud.com>
2023-08-15 11:23:58 +0100

    add some hands tests

--- src/app/actions/hands.ts
+++ src/app/actions/hands.ts
@@ -14,14 +14,65 @@ export enum Rank {
 }
 
 export type Result = {
-  rank: Rank;
+  rank?: Rank;
   cards: Card[];
 };
 
 export function handEvaluator(cards: Card[]): Result {
+  const sorted = cards.sort((a, b) => b.value - a.value);
+
   const result: Result = {
     rank: Rank.HighCard,
-    cards: [cards[0]],
+    cards: [sorted[0]],
   };
   return result;
+}
+
+export function evaluateKinds(cards: Card[]): Result | null {
+  const counts = organiseCardValues(cards);
+
+  if (hasKind(counts, 4)) {
+    return {
+      rank: Rank.FourOfAKind,
+      cards: cards.filter((card) => card.value === hasKind(counts, 4)),
+    };
+  }
+
+  if (hasKind(counts, 3)) {
+    return {
+      rank: Rank.ThreeOfAKind,
+      cards: cards.filter((card) => card.value === hasKind(counts, 3)),
+    };
+  }
+
+  if (hasKind(counts, 2)) {
+    return {
+      rank: Rank.OnePair,
+      cards: cards.filter((card) => card.value === hasKind(counts, 2)),
+    };
+  }
+
+  return null;
+}
+
+function organiseCardValues(cards: Card[]): { [key: string]: Card[] } {
+  const counts: { [key: string]: Card[] } = {};
+  cards.forEach((card) => {
+    if (counts[card.value]) {
+      counts[card.value] = counts[card.value].concat(card);
+    } else {
+      counts[card.value] = [card];
+    }
+  });
+
+  return counts;
+}
+
+function hasKind(obj: { [key: string]: Card[] }, targetValue: number): number {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key) && obj[key].length === targetValue) {
+      return parseInt(key);
+    }
+  }
+  return 0;
 }
--- test/hands.test.ts
+++ test/hands.test.ts
@@ -1,12 +1,58 @@
 import { Card } from "../src/app/actions/cards";
-import { Rank, handEvaluator } from "../src/app/actions/hands";
+import { Rank, evaluateKinds, handEvaluator } from "../src/app/actions/hands";
 import { describe, expect, test } from "@jest/globals";
 
 describe("hand evaluator", () => {
   test("returns high card", () => {
-    const cards: Card[] = [];
+    const cards: Card[] = [
+      new Card(7, "♥️"),
+      new Card(8, "♥️"),
+      new Card(4, "♦️"),
+    ];
     const result = handEvaluator(cards);
 
     expect(result.rank).toBe(Rank.HighCard);
+    expect(result.cards[0].value).toEqual(8);
+  });
+
+  test("returns four of a kind", () => {
+    const card1 = new Card(7, "♥️");
+    const card2 = new Card(7, "♦️");
+    const card3 = new Card(7, "♣️");
+    const card4 = new Card(7, "♠️");
+    const cards: Card[] = [card1, card2, new Card(4, "♦️"), card3, card4];
+
+    const result = evaluateKinds(cards);
+
+    expect(result?.rank).toBe(Rank.FourOfAKind);
+    expect(result?.cards.length).toEqual(4);
+    expect(result?.cards.includes(card1)).toBeTruthy;
+    expect(result?.cards.includes(card2)).toBeTruthy;
+    expect(result?.cards.includes(card3)).toBeTruthy;
+    expect(result?.cards.includes(card4)).toBeTruthy;
+  });
+
+  test("returns a pair", () => {
+    const card1 = new Card(7, "♥️");
+    const card2 = new Card(7, "♦️");
+    const cards: Card[] = [card1, card2, new Card(4, "♦️")];
+    const result = evaluateKinds(cards);
+    expect(result?.rank).toBe(Rank.OnePair);
+    expect(result?.cards.length).toEqual(2);
+    expect(result?.cards.includes(card1)).toBeTruthy;
+    expect(result?.cards.includes(card2)).toBeTruthy;
+  });
+
+  test("returns three of a kind", () => {
+    const card1 = new Card(7, "♥️");
+    const card2 = new Card(7, "♦️");
+    const card3 = new Card(7, "♣️");
+    const cards: Card[] = [card1, card2, new Card(4, "♦️"), card3];
+    const result = evaluateKinds(cards);
+    expect(result?.rank).toBe(Rank.ThreeOfAKind);
+    expect(result?.cards.length).toEqual(3);
+    expect(result?.cards.includes(card1)).toBeTruthy;
+    expect(result?.cards.includes(card2)).toBeTruthy;
+    expect(result?.cards.includes(card3)).toBeTruthy;
   });
 });
