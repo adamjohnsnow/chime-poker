0c8cc1e22ae3085ffe65c966956e560e46240341
Adam Snow <adamjohnsnow@icloud.com>
2023-09-14 08:53:35 +0100

    tidy testing page

--- src/app/testingUi/page.tsx
+++ src/app/testingUi/page.tsx
@@ -2,12 +2,8 @@
 
 import "../styles/table.css";
 
-import { Menu } from "../components/menu";
-
 export default function Test() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-10 font-mono">
-      <Menu controls={undefined} />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-start p-10 font-mono"></main>
   );
 }
