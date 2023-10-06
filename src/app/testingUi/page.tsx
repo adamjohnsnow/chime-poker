84eb0babbed29a1331688d647ff9e7107d384b15
Adam Snow <adamjohnsnow@icloud.com>
2023-10-06 14:06:55 +0100

    testing page imprt

--- src/app/testingUi/page.tsx
+++ src/app/testingUi/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "../styles/table.css";
+import "../../styles/table.css";
 
 import { Menu } from "../../components/menu";
 import { PlayingCard } from "../../components/playingCard";
